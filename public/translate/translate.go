f82946f57ada1ae1524026b63b0e6612095db395
user <ext-dmitriy.arkhangelskiy@sbermarket.ru>
2023-09-05 20:34:39 +0000

    add new translate entity

---
+++ public/translate/translate.go
@@ -0,0 +1,28 @@
+package translate
+
+import (
+	"github.com/wanderer69/MorphologicalSentenceParser/public/natasha"
+	"github.com/wanderer69/MorphologicalSentenceParser/public/relations"
+	"github.com/wanderer69/debug"
+)
+
+type Translate struct {
+	n   *natasha.Natasha
+	rrs *relations.RelationRules
+}
+
+func NewTranslate() *Translate {
+	debug.NewDebug()
+	// debug.LoadFromFile("../../cmd/cli/debug.cfg")
+	n := natasha.NewNatasha("../../scripts/python")
+	rrs := relations.InitRelationRule()
+	return &Translate{
+		n:   n,
+		rrs: rrs,
+	}
+}
+
+func (t *Translate) Translate(sentence string) ([]*relations.TranslateSentensesResultItem, error) {
+	tsris, err := relations.TranslateSentense(t.n, t.rrs, sentence, 0)
+	return tsris, err
+}
